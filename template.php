1a0240b3862db1ddb06ad734bae1f39c0a93c678
Benjamin Chodoroff <ben@theworkdept.com>
2013-08-12 00:02:30 -0400

    only show longer crumbs

--- template.php
+++ template.php
@@ -5,3 +5,10 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * detroitledger.org omega subtheme theme.
  */
+
+function gnl_theme_breadcrumb($vars) {
+  $crumb = $vars['breadcrumb'];
+  if (count($crumb) == 1) {
+    return;
+  }
+}
