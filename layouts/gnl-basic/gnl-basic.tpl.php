a1f778d6cbb2ec13ec28d45e42394eee84e789be
Matt Hampel <matthew.hampel@gmail.com>
2013-08-20 11:49:07 -0400

    stashing stuff

--- layouts/gnl-basic/gnl-basic.tpl.php
+++ layouts/gnl-basic/gnl-basic.tpl.php
@@ -1,19 +1,21 @@
 <div class="l-page">
-  <header class="l-header-wrapper" role="banner">
-    <div class="l-branding site-branding">
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-branding__logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-      <?php endif; ?>
-      <?php if ($site_name): ?>
-        <a href="<?php print $front_page; ?>" class="site-branding__name" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-      <?php endif; ?>
-      <?php if ($site_slogan): ?>
-        <h2 class="site-branding__slogan"><?php print $site_slogan; ?></h2>
-      <?php endif; ?>
-    </div>
-    <?php print render($page['navigation']); ?>
-    <?php print render($page['header']); ?>
-  </header>
+  <div class="full-width-container contrast">
+    <header class="l-header-wrapper" role="banner">
+      <div class="l-branding site-branding">
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-branding__logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+        <?php endif; ?>
+        <?php if ($site_name): ?>
+          <a href="<?php print $front_page; ?>" class="site-branding__name" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+        <?php endif; ?>
+        <?php if ($site_slogan): ?>
+          <h2 class="site-branding__slogan"><?php print $site_slogan; ?></h2>
+        <?php endif; ?>
+      </div>
+      <?php print render($page['navigation']); ?>
+      <?php print render($page['header']); ?>
+    </header>
+  </div>
 
   <?php if (!empty($page['highlighted'])): ?>
     <div class="l-highlighted-wrapper">
