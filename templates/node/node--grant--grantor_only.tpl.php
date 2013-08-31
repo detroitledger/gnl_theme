609d0ccb6b76a3d2229307480c992df60912b44d
Benjamin Chodoroff <ben@theworkdept.com>
2013-08-31 16:14:00 -0400

    templates for grantor and grantee only display modes, refs detroitledger/gnl_theme#5

---
+++ templates/node/node--grant--grantor_only.tpl.php
@@ -0,0 +1,16 @@
+<?php
+$funder = strip_tags($content['field_funder'][0]['#markup']);
+$recipient = strip_tags($content['field_recipient'][0]['#markup']);
+$amount = $content['field_funded_amount'][0]['#markup'];
+$year = $content['field_year'][0]['#markup'];
+?>
+<article<?php print $attributes; ?>>
+  <?php if (!empty($title_prefix) || !empty($title_suffix) || !$page): ?>
+
+      <?php print render($title_prefix); ?>
+      <?php if (!$page): ?>
+          <a href="<?php print $node_url; ?>" rel="bookmark"><?php print $amount; ?> from <?php print $funder; ?></a>, <?php print $year; ?>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+  <?php endif; ?>
+</article>
